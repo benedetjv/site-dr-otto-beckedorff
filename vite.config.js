f6d530d4154e9c0683c36db60280a6bac4438b52
benedetjoao <joaokrtv@gmail.com>
2025-11-19 21:31:15 -0300

    fix: set base for GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+// IMPORTANTE:
+// Troque o nome abaixo PELO NOME EXATO DO SEU REPOSITÓRIO do GitHub Pages.
+// No seu caso: `site-dr-otto-beckedorff`
+export default defineConfig({
+  base: '/site-dr-otto-beckedorff/',
+})
